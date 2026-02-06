0b43bd25d8a5dfc7a27f1b1ff545910cf9d443cc
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-06 11:08:22 -0500

    slice plot doesn't do whole circle

--- src/scattererwhereartthou/plot.py
+++ src/scattererwhereartthou/plot.py
@@ -53,19 +53,38 @@ def sliceplot(mydata, outfilename="swat_slice.png", show=True, rofe=6371):
         print("noting to plot...")
         return
     firstData = mydata["swat"][0]
+    deepest = 0
+    for s in firstData["scatterers"]:
+        if s["scat"].depth > deepest:
+            deepest = s["scat"].depth
+    for a in firstData["backrays"].arrivals:
+        for s in a.pathSegments:
+            for ss in s.segment:
+                if ss.depth > deepest:
+                    deepest = ss.depth
+    deepest = deepest * 1.10 # little bit more
     plt.figure()
     ax = plt.axes(projection='polar')
-    gridlines=ax.grid(True, alpha=.80)
+    print(f"ax.set_rmax({rofe})")
+    print(f"ax.set_rmin({rofe}-{deepest})")
+
     plt.title(f'Scatter:{firstData["traveltime"]} rayp:{firstData["rayparamdeg"]} phase:{firstData["toscatphase"]} - {firstData["fromscatphase"]}')
 
-    plt.scatter(0, 0, marker='v', s=20, color='blue')
     plt.scatter(math.radians(firstData["esdistdeg"]), firstData["eventdepth"], marker='*', s=20, color='blue')
 
     for s in firstData["scatterers"]:
         plt.scatter(math.radians(s["scat"].distdeg), rofe-s["scat"].depth, marker='.', color='tomato')
+
+
+    ax.set_rmax(rofe)
+    ax.set_rmin(rofe-deepest)
+    ax.set_rorigin(0)
+    ax.set_thetamin(0)
+    ax.set_thetamax(firstData["esdistdeg"]*1.05)
+    gridlines=ax.grid(True, alpha=.80)
     plt.savefig(outfilename, dpi=700, bbox_inches='tight', pad_inches=0.1)
     if show:
-        print("Show map")
+        print("Show slice")
         plt.show()
     else:
         return None
