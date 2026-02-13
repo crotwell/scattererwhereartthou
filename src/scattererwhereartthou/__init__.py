65ccb02f0aff3718c3d19928ae509b2368b07aad
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-13 11:09:28 -0500

    swat use dataclass

--- src/scattererwhereartthou/__init__.py
+++ src/scattererwhereartthou/__init__.py
@@ -3,9 +3,12 @@ __version__="0.0.1"
 
 from .swat import SWAT
 from .plot import mapplot, sliceplot
+from .swat_result import SwatResult, Scatterer
 
 __all__ = [
     "SWAT",
+    "SwatResult",
+    "Scatterer",
     "spherical",
     "mapplot",
     "sliceplot"
--- src/scattererwhereartthou/plot.py
+++ src/scattererwhereartthou/plot.py
@@ -30,13 +30,13 @@ def mapplot(mydata, outfilename="swat_map.png", show=True):
     ax.add_feature(cfeature.OCEAN, color='lightskyblue')
     ax.add_feature(cfeature.LAND, color="oldlace")
     gridlines=ax.gridlines(draw_labels=True, alpha=.80)
-    plt.title(f'Scatter: rayp:{firstData["rayparamdeg"]} phase:{firstData["toscatphase"]} - {firstData["fromscatphase"]}  {makeBazTitle(firstData)}')
+    plt.title(f'Scatter: rayp:{firstData.rayparamdeg} phase:{firstData.toscatphase} - {firstData.fromscatphase}  {makeBazTitle(firstData)}')
 
-    plt.scatter(firstData["stalon"], firstData["stalat"], marker='v', s=20, color='blue')
-    plt.scatter(firstData["evtlon"], firstData["evtlat"], marker='*', s=20, color='blue')
+    plt.scatter(firstData.stalon, firstData.stalat, marker='v', s=20, color='blue')
+    plt.scatter(firstData.evtlon, firstData.evtlat, marker='*', s=20, color='blue')
 
-    for s in firstData["scatterers"]:
-        plt.scatter(s["scat"].lon, s["scat"].lat, marker='.', color='tomato')
+    for s in firstData.scatterers:
+        plt.scatter(s.scat.lon, s.scat.lat, marker='.', color='tomato')
     plt.savefig(outfilename, dpi=700, bbox_inches='tight', pad_inches=0.1)
     if show:
         print("Show map")
@@ -54,33 +54,28 @@ def sliceplot(mydata, outfilename="swat_slice.png", show=True, rofe=6371):
         return
     firstData = mydata["swat"][0]
     deepest = 0
-    for s in firstData["scatterers"]:
-        if s["scat"].depth > deepest:
-            deepest = s["scat"].depth
-    for a in firstData["backrays"].arrivals:
-        for s in a.pathSegments:
-            for ss in s.segment:
-                if ss.depth > deepest:
-                    deepest = ss.depth
+    for s in firstData.scatterers:
+        if s.scat.depth > deepest:
+            deepest = s.scat.depth
     deepest = deepest * 1.10 # little bit more
     plt.figure()
     ax = plt.axes(projection='polar')
     print(f"ax.set_rmax({rofe})")
     print(f"ax.set_rmin({rofe}-{deepest})")
 
-    plt.title(f'Scatter: rayp:{firstData["rayparamdeg"]} phase:{firstData["toscatphase"]} - {firstData["fromscatphase"]} {makeBazTitle(firstData)}')
+    plt.title(f'Scatter: rayp:{firstData.rayparamdeg} phase:{firstData.toscatphase} - {firstData.fromscatphase} {makeBazTitle(firstData)}')
 
-    plt.scatter(math.radians(firstData["esdistdeg"]), firstData["eventdepth"], marker='*', s=20, color='blue')
+    plt.scatter(math.radians(firstData.esdistdeg), firstData.eventdepth, marker='*', s=20, color='blue')
 
-    for s in firstData["scatterers"]:
-        plt.scatter(math.radians(s["scat"].distdeg), rofe-s["scat"].depth, marker='.', color='tomato')
+    for s in firstData.scatterers:
+        plt.scatter(math.radians(s.scat.distdeg), rofe-s.scat.depth, marker='.', color='tomato')
 
 
     ax.set_rmax(rofe)
     ax.set_rmin(rofe-deepest)
     ax.set_rorigin(0)
     ax.set_thetamin(0)
-    ax.set_thetamax(firstData["esdistdeg"]*1.05)
+    ax.set_thetamax(firstData.esdistdeg*1.05)
     gridlines=ax.grid(True, alpha=.80)
     plt.savefig(outfilename, dpi=700, bbox_inches='tight', pad_inches=0.1)
     if show:
@@ -91,6 +86,6 @@ def sliceplot(mydata, outfilename="swat_slice.png", show=True, rofe=6371):
 
 def makeBazTitle(firstData):
     bazTitle = ""
-    if firstData["bazdelta"] < 180:
+    if firstData.bazdelta < 180:
         bazTitle = f"Baz: {firstData['bazoffset']}+-{firstData['bazdelta']}"
     return bazTitle
