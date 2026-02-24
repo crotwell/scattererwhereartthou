89fde34c6a7fcfbaeffef26da88b46cb0a03670d
Philip Crotwell <crotwell@seis.sc.edu>
2026-02-24 08:16:26 -0500

    example doc comments

--- example_range.py
+++ example_range.py
@@ -3,13 +3,17 @@
 import taup
 from scattererwhereartthou import SWAT, mapplot, sliceplot
 
-
+# location of taup version 3 executable, not needed if already on PATH
 taup_path="../../../seis/TauP/build/install/TauP/bin/taup"
-model="prem"
-evt=(66, 166)
-eventdepth=0
-sta=(-11, 120)
-phase="P"
+
+
+model="prem"    # velocity model
+evt=(66, 166)   # eq lat, lon
+eventdepth=200  # eq depth
+sta=(-11, 120)  # station lat, lon
+phase="P"   # reference phase
+max_dist_step=1.0 # max separation between path scatterers in degrees, default is 2 deg
+
 slowrange=(4.5,4.8,0.2)  # min, max, step
 delayrange=(5,7,1.0)     # min, max, step
 bazoffset=3
@@ -18,7 +22,7 @@ bazdelta=0.5
 
 with taup.TauPServer( taup_path=taup_path) as taupserver:
     print("starting...")
-    params = taup.TimeQuery()
+    params = taup.PathQuery()
     params.model(model)
     params.event(*evt)
     params.sourcedepth(eventdepth)
@@ -31,6 +35,7 @@ with taup.TauPServer( taup_path=taup_path) as taupserver:
         swat = SWAT(taupserver, eventdepth, model=model)
         swat.event(*evt)
         swat.station(*sta)
+        swat.dist_step = max_dist_step
         slow = slowrange[0]
         while slow <= slowrange[1]:
             delay = delayrange[0]
