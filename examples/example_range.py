45f307923e5351e33b96990a4621c6280615df89
Philip Crotwell <crotwell@seis.sc.edu>
2026-09-01 16:54:00 -0400

    ver to 0.0.4, new param for min dist spacing for scat points, add to examples, rename dist_step to max_dist_step

--- examples/example_range.py
+++ examples/example_range.py
@@ -3,8 +3,9 @@
 import taup
 from scattererwhereartthou import SWAT, mapplot, sliceplot
 
+taup_path=None
 # location of taup version 3 executable, not needed if already on PATH
-taup_path="../../../seis/TauP/build/install/TauP/bin/taup"
+#taup_path="../../../seis/TauP/build/install/TauP/bin/taup"
 
 
 model="prem"    # velocity model
@@ -12,7 +13,10 @@ evt=(66, 166)   # eq lat, lon
 eventdepth=200  # eq depth
 sta=(-11, 120)  # station lat, lon
 phase="P"   # reference phase
-max_dist_step=1.0 # max separation between path scatterers in degrees, default is 2 deg
+
+max_dist_step=2.0 # max separation between path scatterers in degrees, default is 2 deg
+min_dist_step=0.01 # min separation between path scatterers in degrees, default is 0 deg
+                  # potential scatterers are tossed if closer than
 
 # observed slownesses at station, usually p_minus, p, p_plus
 # but can be more values for denser search results
@@ -41,7 +45,8 @@ with taup.TauPServer( taup_path=taup_path) as taupserver:
     swat = SWAT(taupserver, eventdepth, model=model)
     swat.event(*evt)
     swat.station(*sta)
-    swat.dist_step = max_dist_step
+    swat.max_dist_step = max_dist_step
+    swat.min_dist_step = min_dist_step
 
     for a in timeResult.arrivals:
         print(f"Arrival: {a}")
